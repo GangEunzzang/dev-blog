450b241ebaf0028d7d67cf62f854cd95038e0a99
이강은 <rkddms123456@gmail.com>
2023-07-01 19:25:45 +0900

    first commit

---
+++ src/main/java/com/devblog/DevBlogApplication.java
@@ -0,0 +1,15 @@
+package com.devblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class DevBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBlogApplication.class, args);
+	}
+
+}
