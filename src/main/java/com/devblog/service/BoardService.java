43f75f222a7c739008318bd917d5334f56a6b5cf
이강은 <rkddms123456@gmail.com>
2023-09-18 00:17:00 +0900

    dockerFile 추가

--- src/main/java/com/devblog/service/BoardService.java
+++ src/main/java/com/devblog/service/BoardService.java
@@ -41,7 +41,6 @@ public class BoardService {
         return boardQueryRepository.findByPage(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
     }
     public Long save(BoardDTO.Request request) {
-        System.out.println("testCommit");
         Board board = boardRepository.save(request.toEntity());
         return board.getId();
     }
