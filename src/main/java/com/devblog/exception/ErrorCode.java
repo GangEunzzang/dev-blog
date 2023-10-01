3c7d39dba907a853e7d17750093aedeeec620865
이강은 <rkddms123456@gmail.com>
2023-10-01 23:23:17 +0900

    JWT refresh Token 추가

--- src/main/java/com/devblog/exception/ErrorCode.java
+++ src/main/java/com/devblog/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     /** 404 NOT_FOUND */
     BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시물이 존재하지 않습니다."),
     EXISTS_EMAIL(HttpStatus.CONFLICT, "중복된 아이디입니다."),
-    NOT_EXISTS_USER(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다.");
+    NOT_EXISTS_USER(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다."),
+
+    INVALID_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 토큰입니다");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
--- src/main/java/com/devblog/jwt/JwtProvider.java
+++ src/main/java/com/devblog/jwt/JwtProvider.java
@@ -1,34 +1,63 @@
 package com.devblog.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.RegisteredClaims;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.devblog.domain.entity.User;
+import com.devblog.domain.repository.UserRepository;
+import com.devblog.exception.CustomException;
+import com.devblog.exception.ErrorCode;
+import io.jsonwebtoken.*;
+import jdk.jfr.Registered;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class JwtProvider {
-
     @Value("spring.jwt.secret")
     private String secretKey;
+    private final Long accessTokenValidTime = 60 * 60 * 1000L; // 1시간
+    private final Long refreshTokenValidTime = 1440 * 60 * 7 * 1000L; // 7일
+    private final String accessHeader = "Authorization";
+    private final String refreshHeader = "Authorization-refresh";
+    private static final String BEARER = "Bearer";
+    private static final String EMAIL_CLAIM = "email";
 
-    // 토큰 유효시간 168 시간(7일)
-    private final long tokenValidTime = 1440 * 60 * 7 * 1000L;
     private final CustomUserDetailsService customUserDetailsService;
+    private final UserRepository userRepository;
 
     // secretKey 를 Base64로 인코딩합니다.
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createAccessToken(String email) {
+        return JWT.create()
+                .withSubject("AccessToken")
+                .withExpiresAt(new Date(new Date().getTime() + accessTokenValidTime))
+                .withClaim(EMAIL_CLAIM, email)
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    public String createRefreshToken() {
+        return JWT.create()
+                .withSubject("RefreshToken")
+                .withExpiresAt(new Date(new Date().getTime() + refreshTokenValidTime))
+                .sign(Algorithm.HMAC512(secretKey));
     }
 
     // JWT 토큰 생성
@@ -39,15 +68,81 @@ public class JwtProvider {
         return Jwts.builder()
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간
-                .setExpiration(new Date(now.getTime() + tokenValidTime)) // 토큰 유효 시간
+                .setExpiration(new Date(now.getTime() + accessTokenValidTime)) // 토큰 유효 시간
                 .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘
                 .compact();
+    }
+
+    public void sendAccessToken(HttpServletResponse response, String accessToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.setHeader(accessHeader, accessToken);
+        log.info("재발급된 Access Token : {}", accessToken);
+    }
+
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken);
+        setRefreshTokenHeader(response, refreshToken);
+        log.info("Access Token, Refresh Token 헤더 설정 완료");
+    }
+
+    public Optional<String> extractRefreshToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(refreshHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    public Optional<String> extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(accessHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
     }
 
     // JWT 토큰에서 인증 정보 조회
     public Authentication getAuthentication(String token) {
         UserDetails userDetails = customUserDetailsService.loadUserByUsername(this.getUserEmail(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // AccessToken에서 Email 추출
+    public Optional<String> extractEmail(String accessToken) {
+        try {
+            // 토큰 유효성 검사하는 데에 사용할 알고리즘이 있는 JWT verifier builder 반환
+            return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
+                    .build() // 반환된 빌더로 JWT verifier 생성
+                    .verify(accessToken) // accessToken을 검증하고 유효하지 않다면 예외 발생
+                    .getClaim(EMAIL_CLAIM)
+                    .asString());
+        } catch (Exception e) {
+            log.error("액세스 토큰이 유효하지 않습니다.");
+            return Optional.empty();
+        }
+    }
+
+    public void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
+        response.setHeader(accessHeader, accessToken);
+    }
+
+    public void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
+        response.setHeader(refreshHeader, refreshToken);
+    }
+
+    public void updateRefreshToken(String email, String refreshToken) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+        user.updateRefreshToken(refreshToken);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
+            return true;
+        } catch (Exception e) {
+            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
+            return false;
+        }
     }
 
     // 토큰에서 회원 정보 추출
